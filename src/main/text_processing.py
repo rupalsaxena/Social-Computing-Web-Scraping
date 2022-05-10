33de12107330e8ef42ca5ca0ac1f23865f83984f
mkorob <91213317+mkorob@users.noreply.github.com>
2022-05-10 22:24:39 +0200

    Create text_processing.py
    Right now this file just takes every file on the data, opens the file and gets array of all real words - I did a filter for NLTK stop words (the, not etc..) and also a filter for things which just contain letters (this removes numbers, wages etc. and punctuation, which I think makes sense- maybe there is an argument that in "8 hours per a day" 8 is a valid word, but idk.... I don't think so personally 
    
    Outstanding points:
    
    1) maybe on a large quality of data, there will be again some issue with special characters, I tested on 50 succesfully, but who knows....
    2) I checked this method on 5 job descriptions, ideally, it would be good to check on more than 5 to see if there is something not removed that should be
    3) What should be the outcome of this module? Kallia and Orestis, do you want to do your part e.g. young words/total words want to work inside this part on each "words" array, or should this export arrays of words and you work on this after? I think the first option is quicker, but the second option allows to look through each array in case there is some odd result, could be nice!

---
+++ src/main/text_processing.py
@@ -0,0 +1,28 @@
+import os
+#import nltk
+#nltk.download('punkt')
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+jobs = ['plumber', 'computer-engineer', 'nurse', 'oil-field-engineer',
+        'clerk', 'graphic-designer', 'doctor', 'bartender', 'Customer-Service-Representative', 'house-cleaner']
+    
+#link example
+path_texts = "data/raw_job_data"
+
+def main():
+    for job_names in jobs:
+        complete_link = path_texts+"/"+job_name
+        for filename in os.listdir(complete_link):
+            text = str(open(complete_link+"/"+str(filename)).readlines())
+            text_tokens = word_tokenize(text)
+            tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
+            
+            # this keeps empty spaces punctuations marks
+            #I put extra filtering for jsut words. this removes also numbers which I think is valid
+            words = list(filter(lambda x: x.isalpha(), tokens_without_sw))
+            
+    
+if __name__ == "__main__":
+    main()
